f4fd0a6de9f29d63a12c92862f7c96e577080022
rawald <diprawal@hotmail.com>
2021-03-28 12:58:54 +0200

    Initial commit. Includes starting code for simple calculator

---
+++ Simple Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string welcomeMessage = "Welcome to the C# calculator";
+
+            string inputNumberOne = Console.ReadLine();
+            int convertedInputNumberOne;
+            int.TryParse(inputNumberOne, out convertedInputNumberOne);
+            
+        }
+    }
+}
